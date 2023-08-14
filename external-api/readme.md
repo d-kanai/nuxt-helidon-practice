4c80844b4f657fdf6acd6e6c5a7cade01787535a
jiadchen <jiadchen@redhat.com>
2023-08-14 15:43:09 +0900

    external apiを追加

---
+++ external-api/readme.md
@@ -0,0 +1,14 @@
+# 起動方法
+./start.sh instance_name port  
+例：  
+```
+./start.sh 3001 3001
+```
+instance_name `3001`のフォルダが作成される。そこにマッピングファイルを配置する。
+
+# 停止方法
+./stop.sh instance_name  
+例：  
+```
+./stop.sh 3001
+```
---
+++ external-api/wiremock/stop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is executed from the tests, so the current working directory is the tests folder.
+# Change to the wiremock directory
+cd ../wiremock
+
+instance=1
+if [ $# -gt 0 ]; then
+    instance=$1
+fi
+pidFile=wiremock.$instance.pid
+
+
+if [ -e $pidFile ]; then
+  kill -9 `cat $pidFile`
+  rm $pidFile
+else
+  echo WireMock is not started 2>&1
+  exit 1
+fi
+
+echo WireMock $instance stopped
