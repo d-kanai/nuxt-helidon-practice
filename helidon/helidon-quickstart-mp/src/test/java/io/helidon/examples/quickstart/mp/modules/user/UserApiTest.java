c27326b01620f14b954eaf5bcf3d60052ac15c5f
jiadchen <jiadchen@redhat.com>
2023-08-29 18:43:36 +0900

    UserApiTestにおいて、userRepositoryのMockを使えるようにした

--- helidon/helidon-quickstart-mp/src/test/java/io/helidon/examples/quickstart/mp/modules/user/UserApiTest.java
+++ helidon/helidon-quickstart-mp/src/test/java/io/helidon/examples/quickstart/mp/modules/user/UserApiTest.java
@@ -1,71 +1,54 @@
 package io.helidon.examples.quickstart.mp.modules.user;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.helidon.examples.quickstart.mp.modules.user.domain.User;
-import io.helidon.microprofile.tests.junit5.HelidonTest;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.MediaType;
+import io.helidon.examples.quickstart.mp.modules.user.dto.UserAddRequest;
+import io.helidon.examples.quickstart.mp.modules.user.dto.UserAddResponse;
 import jakarta.ws.rs.core.Response;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
-@HelidonTest
+@ExtendWith(MockitoExtension.class)
 public class UserApiTest {
-    @Inject
-    private WebTarget target;
-
-    ObjectMapper mapper;
-
     @InjectMocks
-    private UserResource userResource;
+    private UserResource target;
 
     @Mock
     private UserRepository userRepository;
 
-    @BeforeEach
-    public void beforeEach() {
-        MockitoAnnotations.openMocks(this);
-        this.mapper = new ObjectMapper();
-    }
-
     @Test
     void test_ユーザ登録APIを呼び出せること() throws JsonProcessingException {
         // Arrange
-        //language=JSON
-        String expectedResponse = "{\n" +
-                "  \"message\":" +
-                "  \"user is created.\"\n" +
-                "}";
+        UserAddResponse expectedResponse = new UserAddResponse();
+        expectedResponse.setMessage("user is created.");
         User expectedUser = new User();
         expectedUser.setName("jiadong.chen");
         expectedUser.setAge(39);
 
-
         // Act
-        Response r = target
-                .path("api/v1/user")
-                .request()
-                .post(Entity.entity("{\n" +
-                        "  \"name\":" +
-                        "  \"jiadong.chen\",\n" +
-                        "  \"age\": 39\n" +
-                        "}", MediaType.APPLICATION_JSON));
+        UserAddRequest request = new UserAddRequest();
+        request.setName("jiadong.chen");
+        request.setAge(39);
+        Response r = target.addUser(request);
         int actualStatus = r.getStatus();
-        String actualResponse = r.readEntity(String.class);
+        UserAddResponse actualResponse = (UserAddResponse) r.getEntity();
 
         // Assert
         assertThat(actualStatus).isEqualTo(201);
-        assertThat(mapper.readTree(actualResponse)).isEqualTo(mapper.readTree(expectedResponse));
-//        verify(userRepository).addUser(expectedUser);
+        assertThat(actualResponse).usingRecursiveComparison().isEqualTo(expectedResponse);
+
+        // userRepository.addUser()が呼ばれた時の引数をチェック
+        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).addUser(captor.capture());
+        User actualUser = captor.getValue();
+        assertThat(actualUser).usingRecursiveComparison().isEqualTo(expectedUser);
     }
 }
