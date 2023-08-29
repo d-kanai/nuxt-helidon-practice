4dd9d25b7fbf86986383fd90906bbe1589c32e02
jiadchen <jiadchen@redhat.com>
2023-08-29 15:28:56 +0900

    ユーザ登録APIのAPITestを作成した

---
+++ helidon/helidon-quickstart-mp/src/test/java/io/helidon/examples/quickstart/mp/modules/user/UserApiTest.java
@@ -0,0 +1,46 @@
+package io.helidon.examples.quickstart.mp.modules.user;
+
+import io.helidon.microprofile.tests.junit5.HelidonTest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@HelidonTest
+public class UserApiTest {
+    @Inject
+    private WebTarget target;
+
+    @Test
+    void test_ユーザ登録ができること() {
+        // Arrange
+        //language=JSON
+        String expectedResponse = "{\n" +
+                "  \"message\":" +
+                "  \"user is created.\"\n" +
+                "}";
+
+
+        // Act
+        Response r = target
+                .path("api/v1/user")
+                .request()
+                .post(Entity.entity("{\n" +
+                        "  \"name\":" +
+                        "  \"jiadong.chen\",\n" +
+                        "  \"age\": 39\n" +
+                        "}", MediaType.APPLICATION_JSON));
+        int actualStatus = r.getStatus();
+        String actualResponse = r.readEntity(String.class);
+
+        assertThat(actualStatus).isEqualTo(201);
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+
+
+        // Assert
+    }
+}
