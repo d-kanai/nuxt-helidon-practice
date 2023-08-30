18be73b224a887d4835fa55081c819bbbb30138c
jiadchen <jiadchen@redhat.com>
2023-08-30 15:52:26 +0900

    refactoring change to yup

--- frontend/components/__tests__/LoginForm.spec.ts
+++ frontend/components/__tests__/LoginForm.spec.ts
@@ -1,56 +1,11 @@
-import { vi, expect, test, beforeAll, describe, afterEach } from "vitest";
+import { expect, test, describe } from "vitest";
 import { mount } from "@vue/test-utils";
 import flushPromises from "flush-promises";
 import waitForExpect from "wait-for-expect";
 
-import { localize, setLocale } from "@vee-validate/i18n";
-import en from "@vee-validate/i18n/dist/locale/en.json";
-import ja from "@vee-validate/i18n/dist/locale/ja.json";
-import AllRules from "@vee-validate/rules";
-import { defineRule, configure } from "vee-validate";
-
 import LoginForm from "../login-form.vue";
 
 describe("LoginForm.vue", () => {
-  beforeAll(() => {
-    configure({
-      generateMessage: localize({
-        en,
-        ja,
-      }),
-    });
-    Object.keys(AllRules).forEach((rule) => {
-      defineRule(rule, AllRules[rule]); // 全ルールを使えるようにする
-    });
-    defineRule("minLength", (value, [limit]) => {
-      // The field is empty so it should pass
-      if (!value || !value.length) {
-        return true;
-      }
-      if (value.length < limit) {
-        return `${limit}文字以上にしてください`;
-      }
-      return true;
-    });
-
-    defineRule("maxLength", (value, [limit]) => {
-      // The field is empty so it should pass
-      if (!value || !value.length) {
-        return true;
-      }
-      if (value.length > limit) {
-        return `${limit}文字以下にしてください`;
-      }
-      return true;
-    });
-
-    setLocale("ja");
-  });
-
-  afterEach(() => {
-    vi.useRealTimers();
-  });
-
   test("renders without errors", () => {
     const wrapper = mount(LoginForm);
     expect(wrapper.exists()).toBeTruthy();
@@ -67,7 +22,7 @@ describe("LoginForm.vue", () => {
 
     await flushPromises();
     await waitForExpect(() => {
-      expect(wrapper.text()).toContain("emailは必須項目です"); // emailのエラーメッセージが表示されるはず
+      expect(wrapper.text()).toContain("Email is required"); // emailのエラーメッセージが表示されるはず
     });
   });
 
@@ -82,7 +37,7 @@ describe("LoginForm.vue", () => {
 
     await flushPromises();
     await waitForExpect(() => {
-      expect(wrapper.text()).toContain("passwordは必須項目です"); // emailのエラーメッセージが表示されるはず
+      expect(wrapper.text()).toContain("Password is required"); // emailのエラーメッセージが表示されるはず
     });
   });
 });
--- frontend/components/login-form.vue
+++ frontend/components/login-form.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>LoginForm</h3>
     <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors }">
-      <Field name="email" rules="required|email" />
+      <Field name="email" />
       <span>{{ errors.email }}</span>
       <br />
       <Field name="password" type="password" />
@@ -17,12 +17,21 @@
 
 <script setup>
 import { Form, Field } from "vee-validate";
-const schema = {
-  email: "required|email",
-  password: "required|minLength:8|maxLength:16",
-};
+import { object, string } from 'yup';
+import { ref } from 'vue';
 
 const submitting = ref(false);
+
+const schema = object({
+  email: string()
+    .required("Email is required")
+    .email("Invalid email"),
+  password: string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .max(16, "Password must not exceed 16 characters"),
+});
+
 const onSubmit = async () => {
   submitting.value = true;
   // 2秒待つ
