59b3ffd95a39166b551613d55f2e50e6780ee685
jiadchen <jiadchen@redhat.com>
2023-09-20 11:52:18 +0900

    frontendに環境変数を追加

--- frontend/apis/axios-instance.ts
+++ frontend/apis/axios-instance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+const runtimeConfig = useRuntimeConfig();
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: runtimeConfig.public.apiBase as string,
 });
